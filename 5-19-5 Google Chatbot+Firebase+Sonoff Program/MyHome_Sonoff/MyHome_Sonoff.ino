4cb50e8f6c27e4c49ef7d817548bf3178019adc4
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2020-04-21 14:59:00 +0900

    google chatbot sonoff

---
+++ 5-19-5 Google Chatbot+Firebase+Sonoff Program/MyHome_Sonoff/MyHome_Sonoff.ino
@@ -0,0 +1,53 @@
+//참조유튜브  https://www.youtube.com/watch?v=gXXOgSi9umU
+#include <ESP8266WiFi.h>                                                // esp8266 library
+#include <FirebaseArduino.h>                                             // firebase library
+
+#define FIREBASE_HOST "******.firebaseio.com"                         // the project name address from firebase id
+#define FIREBASE_AUTH "************YE6dZlmF2p2yl"                    // the secret key generated from firebase
+#define WIFI_SSID "i2r"                                          // input your home or public wifi name 
+#define WIFI_PASSWORD "00000000"                                    //password of wifi ssid
+
+String fireStatus = "";                                                     // led status received from firebase
+int gpio13Led = 13;
+int gpio12Relay = 12;
+void setup() {
+  pinMode(gpio13Led, OUTPUT);
+  digitalWrite(gpio13Led, HIGH);
+  pinMode(gpio12Relay, OUTPUT);
+  digitalWrite(gpio12Relay, HIGH);
+  Serial.begin(115200);
+  delay(1000);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                      //try to connect with wifi
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP());                                                      //print local IP address
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
+  Firebase.setString("data/on", "0");                                          //send initial string of led status
+}
+
+void loop() {
+  fireStatus = Firebase.getString("data/on");                                      // get ld status input from firebase
+  if (fireStatus == "1") {                                                          // compare the input of led status received from firebase
+    Serial.println("Led Turned ON");                         
+    digitalWrite(gpio13Led, LOW);
+    digitalWrite(gpio12Relay, HIGH);                                                  // make bultin led ON
+  } 
+  else if (fireStatus == "0") {                                                  // compare the input of led status received from firebase
+    Serial.println("Led Turned OFF");
+    digitalWrite(gpio13Led, HIGH);
+    digitalWrite(gpio12Relay,LOW);                                                        // make external led OFF
+  }
+  else {
+    Serial.println("Wrong Credential! Please send ON/OFF");
+  }
+  delay(1000);
+}
